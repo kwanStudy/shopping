bf8d722d4630375b0e157dfcdc168de8a9fb5b76
[kwanyol] <pkl824000@gmail.com>
2022-02-07 19:57:37 +0900

    add purchaseHistory entitl

---
+++ src/main/java/com/kwan/shopping/domain/entity/PurchaseHistory.java
@@ -0,0 +1,31 @@
+package com.kwan.shopping.domain.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class PurchaseHistory extends BaseEntity {
+
+  @Id //PK 설정
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // 시퀀스 !!!
+  private Long purchaseHistoryId;  //wrapper 클래스 ?
+  //  wrapper 클래스를 사용하는 이유 :  Integer / int  Long/ long
+  // character / char   db 의 컬럼값이 null 인경우가 에러가 발생하기때문에
+  // wrapper 클래스를 사욯해야함.
+  // 회색은 아직 사용을 하지않은 것들
+
+  @Column
+  private Long productId;
+
+  @Column
+  private Long purchaseHistoryGroupId;
+
+  @Column
+  private Integer count;
+
+}
---
+++ src/main/java/com/kwan/shopping/domain/repository/PurchaseHistoryRepository.java
@@ -0,0 +1,10 @@
+package com.kwan.shopping.domain.repository;
+
+import com.kwan.shopping.domain.entity.PurchaseHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface  PurchaseHistoryRepository extends JpaRepository<PurchaseHistory , Long> {
+//JpaRepository<PurchaseHistory(이거 쓸꺼야~) , Long(pK키 기준으로 타입정하기)>
+}
